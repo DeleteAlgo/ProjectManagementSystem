2eab1bb37af5e89dcb6420ee07c977da576c7c61
DeleteAlgo <ronald.porras.rasco@gmail.com>
2025-09-04 03:27:04 +0800

    ✨Add React and Tailwind integration to Laravel project

---
+++ resources/js/pages/index.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-800">
+      <h1 className="text-5xl font-bold text-indigo-400">
+        🎉 Tailwind works in React!
+      </h1>
+    </div>
+  )
+}
