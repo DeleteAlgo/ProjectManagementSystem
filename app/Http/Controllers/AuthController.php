eafcc351107429fcef467f802a1be9d2cd3ac376
DeleteAlgo <ronald.porras.rasco@gmail.com>
2025-09-05 01:12:02 +0800

    ✔ Added user data in passing api

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,7 +11,7 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         $credentials = $request->only('email', 'password');
-
+        
         if (Auth::attempt($credentials)) {
             $user = Auth::user();
             $token = $user->createToken('auth_token')->plainTextToken;
@@ -19,6 +19,7 @@ class AuthController extends Controller
             return response()->json([
                 'access_token' => $token,
                 'token_type' => 'Bearer',
+                'user' => $user
             ]);
         }
 
