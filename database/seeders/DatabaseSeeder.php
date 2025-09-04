ccc7a5152532c5797fdfd95c7bf4a863f9ca1865
DeleteAlgo <ronald.porras.rasco@gmail.com>
2025-09-05 05:00:48 +0800

    Updated DatabaseSeeder for accurate data seeding and factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Department;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,10 +14,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $this->call([
+            RoleSeeder::class,
+            DepartmentSeeder::class,
+        ]);
 
-        User::factory()
-        ->count(10)
-        ->create();
+        $departments = Department::all();
+
+        // Create users for each department
+        foreach ($departments as $department) {
+            $users = User::factory(3)->create([
+                'department_id' => $department->id,
+            ]);
+
+            // Assign a random user as manager for this department
+            $department->manager_id = $users->random()->id;
+            $department->save();
+        }       
     }
 }
