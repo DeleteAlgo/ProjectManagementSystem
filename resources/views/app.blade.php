5fa1d8e985730fcc66ad0f99533c40798106dab5
DeleteAlgo <ronald.porras.rasco@gmail.com>
2025-09-05 01:14:18 +0800

    ✔ add crsf token on meta tag

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,11 +3,12 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
     @viteReactRefresh
     @vite(['resources/css/app.css', 'resources/js/app.jsx'])
     @inertiaHead
+    <script src="http://localhost:8097"></script>
 </head>
 <body class="antialiased">
     @inertia
